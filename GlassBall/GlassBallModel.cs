5a19b5571cb09d751a8a47547c43c9a025a3ec3e
WDot <mad6384@rit.edu>
2015-08-30 16:27:57 -0400

    Glass Ball project

---
+++ GlassBall/GlassBallModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlassBall
+{
+    class GlassBallModel
+    {
+        private int radius;
+        private R2<int,int> coords;
+        private R2<double, double> vector;
+        public int Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+        public int CoordX
+        {
+            get { return coords.X1; }
+            set { coords.X1 = value; }
+        }
+        public int CoordY
+        {
+            get { return coords.X2; }
+            set { coords.X2 = value; }
+        }
+
+        public double VelocityMagnitude
+        {
+            get { return vector.X1; }
+            set { vector.X1 = value; }
+        }
+        public double VelocityAngle
+        {
+            get { return vector.X2; }
+            set { vector.X2 = value; }
+        }
+
+        public GlassBallModel(int _radius, int _coordX, int _coordY)
+        {
+            radius = _radius;
+            CoordX = _coordX;
+            CoordY = _coordY;
+            VelocityMagnitude = 0;
+            VelocityAngle = 0;
+        }
+    }
+}
---
+++ GlassBall/R2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlassBall
+{
+    public class R2<T, U>
+    {
+        private T x1;
+        private U x2;
+
+        public T X1
+        {
+            get { return x1; }
+            set { x1 = value; }
+        }
+
+        public U X2
+        {
+            get { return x2; }
+            set { x2 = value; }
+        }
+
+        R2(T _x, U _y)
+        {
+            x1 = _x;
+            x2 = _y;
+        }
+    }
+}
